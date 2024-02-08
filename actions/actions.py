b18f05c356acadc65f54cf4b3bac640f82a18226
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2024-02-08 17:03:43 -0300

    rasa updated

--- actions/actions.py
+++ actions/actions.py
@@ -5,6 +5,7 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 import requests
 from typing import Any, Text, Dict, List, Union
+import re
 
 class ActionGetAtendimentos(Action):
     print("============teste da API===========")    
@@ -13,46 +14,32 @@ class ActionGetAtendimentos(Action):
         return "action_get_atendimentos"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        atendimentos = [
-            {"nrAtendimento":13919,"nmMedico":"Williams Fernandes Barra","dtEntrada":"2023-11-29","dsConvenio":"Unimed Belém"},
-            {"nrAtendimento":13921,"nmMedico":"Williams Fernandes Barra","dtEntrada":"2023-11-29","dsConvenio":"Unimed Belém"},
-            {"nrAtendimento":13923,"nmMedico":"Sâmio Pimentel Ferreira","dtEntrada":"2023-11-29","dsConvenio":"Unimed Belém"},
-            {"nrAtendimento":13926,"nmMedico":"Sâmio Pimentel Ferreira","dtEntrada":"2023-11-29","dsConvenio":"Aeronáutica"},
-            {"nrAtendimento":13927,"nmMedico":"Sâmio Pimentel Ferreira","dtEntrada":"2023-11-29","dsConvenio":"CASSI"},
-            {"nrAtendimento":13928,"nmMedico":"LORENA CUNHA CASTRO PATRIOTA","dtEntrada":"2023-11-29","dsConvenio":"Bradesco"},
-            {"nrAtendimento":13930,"nmMedico":"Manuel Caitano Dias Ferreira Maia","dtEntrada":"2023-11-29","dsConvenio":"Unimed Belém"},
-            {"nrAtendimento":13929,"nmMedico":"LORENA CUNHA CASTRO PATRIOTA","dtEntrada":"2023-11-29","dsConvenio":"Bradesco"}
-        ]
-
-        intent = tracker.latest._message['intent']['name']
-
-        if intent == 'informacoes_atendimento':
-            # Lógica para extrair informações do atendimento com base nos parâmetros fornecidos
-            nr_atendimento = tracker.latest_message['entities'][0]['value']
-            info_atendimento = next((atendimento for atendimento in atendimentos if atendimento["nrAtendimento"] == nr_atendimento), None)
-
-            if info_atendimento:
-                # Se encontrou o atendimento, envie as informações para o usuário
-                dispatcher.utter_message(f"Informações do atendimento {nr_atendimento}: {info_atendimento}")
+        api_url = "http://localhost:8080/bot/getAtendimentos"
+        response = requests.get(api_url)
+        # Imprimir o conteúdo completo da mensagem do usuário
+        print("Conteúdo da mensagem:", tracker.latest_message)
+        
+        if response.status_code == 200:
+            # Extrair os dados da resposta JSON
+            atendimentos = response.json()
+            
+            # Extrair o nome do médico usando a entidade
+            entities = tracker.latest_message.get('entities')
+            if entities:
+                for entity in entities:
+                    if entity['entity'] == 'nmMedico':
+                        nome_medico = entity['value']
+            
+                        atendimentos_medico = [atendimento for atendimento in atendimentos if atendimento["nmMedico"] == nome_medico]
+                        if atendimentos_medico:
+                            numeros_atendimentos = [str(atendimento["nrAtendimento"]) for atendimento in atendimentos_medico]
+                            dispatcher.utter_message(f"Atendimentos do Dr. {nome_medico}: {atendimentos_medico}")
+                        else:
+                            dispatcher.utter_message(f"Atendimentos do Dr. {nome_medico} não encontrados.")
             else:
-                # Se não encontrou o atendimento, informe ao usuário
-                dispatcher.utter_message(f"Atendimento {nr_atendimento} não encontrado.")
-
-        elif intent == 'quantidade_atendimentos':
-            # Lógica para calcular a quantidade de atendimentos e enviar ao usuário
-            quantidade_atendimentos = len(atendimentos)
-            dispatcher.utter_message(f"Total de atendimentos: {quantidade_atendimentos}")
-
-        elif intent == 'medico_atendimentos':
-            # Lógica para extrair informações do médico e enviar ao usuário
-            nome_medico = tracker.latest_message['entities'][0]['value']
-            atendimentos_medico = [atendimento for atendimento in atendimentos if atendimento["nmMedico"] == nome_medico]
-
-            if atendimentos_medico:
-                # Se encontrou atendimentos do médico, envie as informações para o usuário
-                dispatcher.utter_message(f"Atendimentos do Dr. {nome_medico}: {atendimentos_medico}")
-            else:
-                # Se não encontrou atendimentos do médico, informe ao usuário
-                dispatcher.utter_message(f"Atendimentos do Dr. {nome_medico} não encontrados.")
-
+                dispatcher.utter_message("Por favor, forneça o nome do médico.")
+        else:
+            # Se houver um erro na solicitação, envie uma mensagem de erro
+            dispatcher.utter_message("Ocorreu um erro ao obter os dados da API.")
+        
         return []
